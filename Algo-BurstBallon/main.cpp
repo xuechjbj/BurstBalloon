bf4b741f71048e9fc95a36e983a6036cab7698c5
Chaojun Xue <xuechaojun88@gmail.com>
2016-01-11 13:15:07 -0800

    Initial Commit

---
+++ Algo-BurstBallon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algo-BurstBallon
+//
+//  Created by Chaojun Xue on 1/11/16.
+//  Copyright © 2016 Chaojun Xue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
